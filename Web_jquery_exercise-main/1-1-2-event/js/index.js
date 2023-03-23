13c9e7806f8f337ed2568025f8ba0f68bfb8aeb3
wuchrchi <sq12348338@gmail.com>
2023-03-23 18:57:51 +0800

    change

---
+++ Web_jquery_exercise-main/1-1-2-event/js/index.js
@@ -0,0 +1,12 @@
+// $('h1').on('click',function({
+//     alert('hello world')
+// }));
+$('h1').click (function(){
+    alert('hello world')
+});
+
+$('p').click();
+
+function saysomething(){
+    alert('hello')
+}
